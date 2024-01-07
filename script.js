c0180e26d99f0af470a03b2ca47fcda552fddbfc
sabaghip <sabaghi.p@gmail.com>
2024-01-07 11:45:08 +0330

    refactor : comments added

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let cookie = document.cookie
 
 let cache = {}
 
+// processed cookie to get saved answers
 if(cookie.split("-").length > 1){
     for(i of cookie.split("-")){
         let temp = i.split(":")
@@ -9,53 +10,64 @@ if(cookie.split("-").length > 1){
     }
 }
 
-
+// function to check foem and send request to other site and show result
 async function checkform(event) {
+    // show message to user in result field
     document.getElementById("result-string").innerHTML="wait for answer"
     document.getElementById("result-percent").innerHTML=""
+    // prevent form to submit by default
     event.preventDefault();
     let name = document.getElementById("name-field");
+    // use regex to check name
     reg_expr = /^[A-Za-z\s]*$/;
     if(!reg_expr.test(name.value)){
+        // show message to user in result field if name is invalid
         document.getElementById("result-string").innerHTML="invalid name"
         document.getElementById("result-percent").innerHTML=""
         return
     }
     if(name.length > 255){
+        // show message to user in result field if name has more than 255 chars
         document.getElementById("result-string").innerHTML="invalid name"
         document.getElementById("result-percent").innerHTML=""
         return
     }
+    // show result to user in saved answers field if name is in cache
     if(["male", "female"].includes(cache[name.value])){
         document.getElementById("saved-answer").innerHTML = cache[name.value]
     }
+    // clear result in saved answers field if name is not in cache
     else{
         document.getElementById("saved-answer").innerHTML = ""
     }
     try{
+        // send request for result
         const response = await fetch('https://api.genderize.io/?' + new URLSearchParams({
                 name: name.value,
             }));
         const data = await response.json();
+        //if result has gender show that to user in result field
         if(data.gender){
             document.getElementById("result-string").innerHTML=data.gender
             document.getElementById("result-percent").innerHTML=data.probability
         }
+        //if result doesnt have gender show message to user in result field
         else{
             document.getElementById("result-string").innerHTML="cant predict gender"
             document.getElementById("result-percent").innerHTML="for this name"
         }
     }  
+    //if cant send request show message to user in result field
     catch(err){
-        document.getElementById("result-string").innerHTML="connection problem"
+        document.getElementById("result-string").innerHTML="cant send request"
         document.getElementById("result-percent").innerHTML="check connection"
-
         return
     }
-  }
-
+}
+  
+// function to save a saved answer to cach and cookie
 async function save(event) {
-    event.preventDefault();
+    // get name field and check if name is valid(like checkform)
     let name = document.getElementById("name-field");
     reg_expr = /^[A-Za-z\s]*$/;
     if(!reg_expr.test(name.value)){
@@ -68,6 +80,7 @@ async function save(event) {
         document.getElementById("result-percent").innerHTML=""
         return
     }
+    // get radio buttons result
     let gender = null;
     for(i of document.getElementsByClassName("radio-button")){
         if(i.checked){
@@ -75,6 +88,7 @@ async function save(event) {
             break
         }
     }
+    //update cache and cookie if a radio button is selected
     if(gender){
         cache[name.value] = gender
         document.cookie=""
@@ -82,10 +96,11 @@ async function save(event) {
             document.cookie = document.cookie + key + ":" + value + "-"
         }
     }
-  }
+}
 
-  async function clear(event) {
-    event.preventDefault();
+// function to delete a saved answer from cach and cookie
+async function clear(event) {
+    // get name field and check if name is valid(like checkform)
     let name = document.getElementById("name-field");
     reg_expr = /^[A-Za-z\s]*$/;
     if(!reg_expr.test(name.value)){
@@ -98,13 +113,16 @@ async function save(event) {
         document.getElementById("result-percent").innerHTML=""
         return
     }
+    //remove saved answer from cache and cookie
     cache[name.value]=null
     document.cookie=""
     for(const [key, value] of Object.entries(cache)){
         document.cookie = document.cookie + key + ":" + value + "-"
     }
     document.getElementById("saved-answer").innerHTML = ""
-  }
+}
+
+//add listeners to buttons 
 document.getElementById("main-form").onsubmit = checkform;
 
 document.getElementById("save-button").onclick = save;
